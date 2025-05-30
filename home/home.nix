59c51e22e81c227819bec92c3ac5588655dc7ea5
Calcium <masteroffoce@gmail.com>
2025-05-30 20:34:41 +0200

    Added config file for Kmonad in home-manager. I think it will work this time.

--- home/home.nix
+++ home/home.nix
@@ -7,6 +7,7 @@
   programs.home-manager.enable = true;
 
   xdg.configFile."hypr/hyprland.conf".source = ./hypr/hyprland.conf;
+  xdg.configFile."kmonad/test.kbd".source = ./kmonad/test.kbd;
 
   home.packages = with pkgs; [
     kitty
