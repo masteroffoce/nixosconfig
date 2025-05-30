6560f4bded601da9d540a5b119bceb955b024518
Calcium <masteroffoce@gmail.com>
2025-05-30 17:48:03 +0200

    Added pkg-config. Maybe I will have installed all dependencies one day...

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -68,6 +68,7 @@
   environment.systemPackages = with pkgs; [
   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   gcc
+  pkg-config
   gtk3
   #  wget
   ];
