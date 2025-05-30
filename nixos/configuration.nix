92dccc81fcfe5923aea3007f83860e4f200f66f5
Calcium <masteroffoce@gmail.com>
2025-05-30 17:51:45 +0200

    Removed GCC and pkg-manager. Other priorities.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -68,8 +68,6 @@
   environment.systemPackages = with pkgs; [
   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   gcc
-  pkg-config
-  gtk3
   #  wget
   ];
 
