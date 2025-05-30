352e86ea71f909b090876adcebcec34a92821768
Calcium <masteroffoce@gmail.com>
2025-05-30 17:44:32 +0200

    Added GCC

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -67,6 +67,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  gcc
   gtk3
   #  wget
   ];
