bf2ac1a712f1d0bdb4f4cf2d131df9a1b9dc1355
Calcium <masteroffoce@gmail.com>
2025-05-31 08:02:05 +0200

    New reference to .config/kmonad/test.kbd. From homeDirectory declared in home.nix.

--- home/home.nix
+++ home/home.nix
@@ -14,7 +14,7 @@
     keyboards = {
       myKmonadOutput = {
         device = "usb-Keychron_Keychron_Link-if02-event-kbd";
-        config = builtins.readFile ~/.config/kmonad/test.kbd
+        config = builtins.readFile "${config.home.homeDirectory}/.config/kmonad/test.kbd
       };
     };
   };
