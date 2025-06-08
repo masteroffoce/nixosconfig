123539cee819ae803a3b41e53a5191836dae8769
Calcium <masteroffoce@gmail.com>
2025-06-08 16:22:55 +0200

    Fixed typo in home.nix.

--- home/home.nix
+++ home/home.nix
@@ -8,7 +8,7 @@
 
   xdg.configFile."hypr/hyprland.conf".source = ./hypr/hyprland.conf;
   xdg.configFile."hypr/shaders/test1.glsl".source = ./hypr/shaders/test1.glsl;
-  xdg.configGile."hypr/hyprpaper.conf".source = ./hypr/hyprpaper.conf;
+  xdg.configFile."hypr/hyprpaper.conf".source = ./hypr/hyprpaper.conf;
 
   home.file.".bashrc".source = ./.bashrc;
   home.file.".bash_profile".source = ./.bash_profile;
