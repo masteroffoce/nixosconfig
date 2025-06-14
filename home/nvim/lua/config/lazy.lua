fd4ab28584503bb6c9916753f6b0e5aa69724918
Calcium <masteroffoce@gmail.com>
2025-06-14 09:03:13 +0200

    Changed lockfile directory to somewhere where it isn't write protected.

--- home/nvim/lua/config/lazy.lua
+++ home/nvim/lua/config/lazy.lua
@@ -32,4 +32,5 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+  lockfile = vim.fn.stdpath("data") .."/lazy-lock.json",
 })
