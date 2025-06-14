edf35a93f2ef38029c3c46a9b673a99f9fa67f17
Calcium <masteroffoce@gmail.com>
2025-06-14 08:58:36 +0200

    Changed treesitter.lua to something that will hopefully work.

--- home/nvim/lua/plugins/treesitter.lua
+++ home/nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,14 @@
-require("lazy").setup({
-  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
-})
+-- ~/.config/nvim/lua/config/plugins/treesitter.lua
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "python", "c" },
+        highlight = { enable = true },
+      })
+    end,
+  },
+}
+
