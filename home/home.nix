30c7e7ea0b23e37fb1a9c81207e5686d2b864de1
Calcium <masteroffoce@gmail.com>
2025-07-28 17:02:38 +0200

    Let's try again

--- home/home.nix
+++ home/home.nix
@@ -38,7 +38,12 @@
 	nwg-icon-picker
 	qutebrowser
 	librewolf
-	steam
     ];
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+	}
 
 }
