db4b1cc30b3d75aa31c5dfd4445497ec47637e94
Calcium <masteroffoce@gmail.com>
2025-07-28 17:03:34 +0200

    SEMICOLONS ARE LIKE DOLLAR SIGNS IN PHP

--- home/home.nix
+++ home/home.nix
@@ -44,6 +44,6 @@
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-	}
+	};
 
 }
