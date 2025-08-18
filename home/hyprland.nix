d19b170ba7924dfd83da67855f53ea6e43c3025d
Calcium <masteroffoce@gmail.com>
2025-08-18 18:00:38 +0200

    To line 107.

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -16,6 +16,22 @@
       "exec-once" = "cd ~/working_projects/daily_images; ./run.sh";
 
       env = "HYPRCURSOR_SIZE,24";
+
+      general = {
+        "gaps_in" = 5;
+        "gaps_out" = 20;
+
+        "border_size" = 2;
+        
+        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
+
+        "resize_on_border" = "false";
+        "allow_tearing" = "false";
+
+        "layout" = "dwindle";
+      };
+
     };
 
   };
