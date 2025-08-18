f02e00462fbaf5701766a5b0c51efdd68d8e0fa3
Calcium <masteroffoce@gmail.com>
2025-08-18 18:06:58 +0200

    Adding missed =

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -39,13 +39,13 @@
           active_opacity = 1.0;
           inactive_opacity = 1.0;
 
-          shadow {
+          shadow = {
               enabled = true;
               range = 4;
               render_power = 3;
               color = rgba(1a1a1aee);
           };
-          blur {
+          blur = {
               enabled = true;
               size = 3;
               passes = 1;
