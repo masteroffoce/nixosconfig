dc0876b29bcf0f8ae676ecdb354ec5574a4f4be7
Calcium <masteroffoce@gmail.com>
2025-08-18 17:42:41 +0200

    To line 61 in hyprland.conf.

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -7,6 +7,15 @@
       animations = {
         enabled = "no";
       };
+
+      monitor = ",prefered,auto,1";
+
+      "$terminal" = kitty
+      "$menu" = wofi --show drun
+
+      "exec-once" = "cd ~/working_projects/daily_images; ./run.sh"
+
+      env = "HYPRCURSOR_SIZE,24"
     };
 
   };
