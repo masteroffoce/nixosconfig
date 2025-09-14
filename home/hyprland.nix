6bb2582258c1948da46f20ba9960888a0ca00f7c
Calcium <masteroffoce@gmail.com>
2025-09-14 16:43:46 +0200

    Fixed shortcut for pion.

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -111,7 +111,7 @@
         "SUPER, J, togglesplit"
         "SUPER, G, exec, librewolf"
         "SUPER, F, fullscreen"
-        "SUPER, T, exec, ~/active_projects/pion/run"
+        "SUPER, T, exec, ~/active_projects/pion/result/bin/exec"
 
         "SUPER, N, movefocus, l"
         "SUPER, E, movefocus, d"
